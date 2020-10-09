7f8719793d38b9a291148a5210b6d497a48626e1
Grigorii Kostarev <grik2001@gmail.com>
2020-10-09 22:45:10 +0300

    Made and configured frontend project

---
+++ Frontend/.eslintrc.js
@@ -0,0 +1,39 @@
+module.exports = {
+  extends: ['plugin:@angular-eslint/recommended'],
+  rules: {
+    '@angular-eslint/directive-selector': [
+      'error',
+      { type: 'attribute', prefix: 'app', style: 'camelCase' },
+    ],
+    '@angular-eslint/component-selector': [
+      'error',
+      { type: 'element', prefix: 'app', style: 'kebab-case' },
+    ],
+  },
+  overrides: [
+    // Добавьте эти настройки, если вы задаёте шаблоны внутри файлов *.component.ts
+    {
+      files: ['*.component.ts'],
+      parser: '@typescript-eslint/parser',
+      parserOptions: {
+        ecmaVersion: 2020,
+        sourceType: 'module',
+      },
+      plugins: ['@angular-eslint/template'],
+      processor: '@angular-eslint/template/extract-inline-html',
+    },
+    {
+      files: ['*.ts'],
+      extends: [
+        'airbnb-typescript/base',
+        'prettier/@typescript-eslint',
+        'plugin:prettier/recommended',
+      ],
+      parser: '@typescript-eslint/parser',
+      parserOptions: {
+        ecmaVersion: 2020,
+        sourceType: 'module',
+      },
+    },
+  ],
+};
---
+++ Frontend/src/app/app.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: ` <router-outlet></router-outlet> `,
+})
+// eslint-disable-next-line import/prefer-default-export
+export class AppComponent {}
