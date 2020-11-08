459be67f9d87f8e7b35dba2277299cb990282d77
Grigorii Kostarev <grik2001@gmail.com>
2020-11-08 15:59:55 +0300

    error when create teacher

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,14 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
-import { HttpClientModule } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 import { NavigationPanelComponent } from './module/navigation-panel/navigation-panel.component';
-import {ButtonModule} from 'primeng/button';
-import {RippleModule} from 'primeng/ripple';
+import { ButtonModule } from 'primeng/button';
+import { RippleModule } from 'primeng/ripple';
+import { AuthInterceptor } from './interceptor/auth.interceptor';
 
 @NgModule({
   declarations: [AppComponent, NavigationPanelComponent],
   imports: [BrowserModule, AppRoutingModule, HttpClientModule, ButtonModule, RippleModule],
+  // providers: [
+  //   {
+  //     provide: HTTP_INTERCEPTORS,
+  //     useClass: AuthInterceptor,
+  //     multi: true,
+  //   },
+  // ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
---
+++ frontend/src/app/module/control-panel/course-creation/course-creation/course-creation.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-course-creation',
+  templateUrl: './course-creation.component.html',
+  styleUrls: ['./course-creation.component.css']
+})
+export class CourseCreationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
