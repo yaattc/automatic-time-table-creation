db5ec6c481036e0ce445d432ed323524ab24b270
Yelshat Duskaliyev <ura2178@gmail.com>
2020-11-23 10:31:08 +0300

    make study year id reference in courses nullable in order to keep the data in db

--- backend/migrations/20201123095542_course_study_year.sql
+++ backend/migrations/20201123095542_course_study_year.sql
@@ -1,6 +1,6 @@
 -- +goose Up
 -- +goose StatementBegin
-ALTER TABLE courses ADD COLUMN study_year_id UUID NOT NULL DEFAULT '00000000-0000-0000-0000-000000000001';
+ALTER TABLE courses ADD COLUMN study_year_id UUID;
 ALTER TABLE courses ADD CONSTRAINT FK_study_year FOREIGN KEY (study_year_id) REFERENCES study_years(id) ON DELETE CASCADE ;
 -- +goose StatementEnd
 
